160740ceb874cf80eb452458fc2e13be5fca89d8
vieron <javiersanchezmarin@gmail.com>
2011-07-19 22:18:24 +0200

    stylesheets and javascript folders moved to content/assets/** and the output is genereted at output/assets/**

--- compass/config.rb
+++ compass/config.rb
@@ -4,11 +4,11 @@ require "omg-text"
 project_path = File.dirname(__FILE__)
 http_path    = '../'
 output_style = :compact
-sass_dir     = '../content/stylesheets'
-css_dir      = '../output/stylesheets'
+sass_dir     = '../content/assets/stylesheets'
+css_dir      = '../output/assets/stylesheets'
 
-images_dir = "../output/images"
-javascripts_dir = "output/javascripts"
+images_dir = "../output/assets/images"
+javascripts_dir = "output/assets/javascripts"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
